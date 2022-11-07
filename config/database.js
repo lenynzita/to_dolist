c80212d404a4e3c4a774c0e61a932668cfdfe014
Alain Makoso (Leny) <106040804+lenynzita@users.noreply.github.com>
2022-11-07 00:34:45 -0500

    todolistdemo

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+    'url' : 'mongodb+srv://lenynzita:Charisma1@cluster0.uutofyf.mongodb.net/messages?retryWrites=true&w=majority'
+};
+
+//mongodb+srv://lenynzita:<password>@cluster0.uutofyf.mongodb.net/?retryWrites=true&w=majority
